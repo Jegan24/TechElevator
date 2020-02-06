d151fb1b1b8ce4a46b91e0c2b915af43679b5eae
Jack Egan <jackegan2424@gmail.com>
2020-02-06 15:04:23 -0500

    forgot to push

--- module-1/18_FileIO_Writing_out/student-exercise/dotnet/FileSplitter/Program.cs
+++ module-1/18_FileIO_Writing_out/student-exercise/dotnet/FileSplitter/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FileSplitter
 {
@@ -6,7 +9,141 @@ namespace FileSplitter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<string> fileContents;
+            Queue<List<string>> newFiles = new Queue<List<string>>();
+            string originalFilePath;
+            string originalFileName;
+            string directory;
+            int maxFileSize;
+
+            originalFilePath = GetFilePath();
+            if (originalFilePath.Length > 0)
+            {
+                maxFileSize = GetMaxFileSize();
+                fileContents = ReadFile(originalFilePath);
+                directory = originalFilePath.Substring(0, originalFilePath.LastIndexOf("\\") + 1);
+                originalFileName = originalFilePath.Substring(originalFilePath.LastIndexOf("\\"));
+                originalFileName = originalFileName.Remove(originalFileName.IndexOf("."));
+                CreateNewFiles(fileContents, newFiles, maxFileSize);
+                WriteNewFiles(newFiles, originalFileName, directory);
+            }
+
+        }
+
+        public static string GetFilePath()
+        {
+            string filePath = "";
+            bool isValidFilePath;
+
+            Console.WriteLine("Where is the input file (please include the path to the file) ");
+            filePath = Console.ReadLine();
+
+            isValidFilePath = ValidateFilePath(filePath);
+
+            if (!isValidFilePath)
+            {
+                // to do
+                Console.WriteLine("The specified file path was invalid, would you re-enter it? Y/N");
+                string response = Console.ReadLine();
+                if (response.ToUpper() == "Y")
+                {
+                    filePath = GetFilePath();
+                }
+                else
+                {
+                    filePath = "";
+                }
+            }
+
+            return filePath;
+        }
+        public static bool ValidateFilePath(string filePath)
+        {
+            bool isValid = File.Exists(filePath);
+            return isValid;
+        }
+
+        public static int GetMaxFileSize()
+        {
+            int maxSize = 0;
+            string input;
+            Console.Write("How many lines of text (max) should there be in the split files? ");
+            input = Console.ReadLine();
+            if (!(int.TryParse(input, out maxSize)) || maxSize < 1)
+            {
+                Console.WriteLine("Please enter a number greater than 0");
+                maxSize = GetMaxFileSize();
+            }
+            return maxSize;
+        }
+
+        public static Queue<string> ReadFile(string filePath)
+        {
+            Queue<string> fileContents = new Queue<string>();
+            try
+            {
+                //Open a StreamReader with the using statement
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    // Read the file until the end of the stream is reached
+                    // EndOfStream is a "marker" that the stream uses to determine 
+                    // if it has reached the end
+                    // As we read forward the marker moves forward like a typewriter.        
+                    while (!sr.EndOfStream)
+                    {
+                        fileContents.Enqueue(sr.ReadLine());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(ex.Message);
+            }
+
+            return fileContents;
+        }
+
+        public static void CreateNewFiles(Queue<string> originalFile, Queue<List<string>> newFiles, int maxFileSize)
+        {
+            while (originalFile.Count > 0)
+            {
+                newFiles.Enqueue(CreateNewFileChunk(originalFile, maxFileSize));
+            }
+        }
+
+        public static List<string> CreateNewFileChunk(Queue<string> originalFile, int maxFileSize)
+        {
+            List<string> fileChunk = new List<string>();
+            int stoppingPoint = maxFileSize > originalFile.Count ? originalFile.Count : maxFileSize;
+            for (int i = 0; i < stoppingPoint; i++)
+            {
+                fileChunk.Add(originalFile.Dequeue());
+            }
+            return fileChunk;
+        }
+
+        public static void WriteNewFiles(Queue<List<string>> newFiles, string originalFileName, string directory)
+        {
+            int fileCounter = 1;
+
+            while (newFiles.Count > 0)
+            {
+                WriteNewFile(newFiles.Dequeue(), fileCounter, originalFileName, directory);
+                fileCounter++;
+            }
+        }
+
+        public static void WriteNewFile(List<string> newFile, int fileNumber, string originalFileName, string directory)
+        {
+
+            using (StreamWriter sw = new StreamWriter($"{directory}{originalFileName}-{fileNumber}.txt", false))
+            {
+                foreach (string line in newFile)
+                {
+                    sw.WriteLine(line);
+                }
+            }
         }
     }
 }
--- module-1/18_FileIO_Writing_out/student-exercise/dotnet/FizzWriter/Program.cs
+++ module-1/18_FileIO_Writing_out/student-exercise/dotnet/FizzWriter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace FizzWriter
 {
@@ -6,7 +8,81 @@ namespace FizzWriter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string contents = GetFileContents();
+
+            WriteToFile(contents);
+        }
+
+        public static string GetFileContents()
+        {
+            string output = "";
+
+            for (int i = 1; i < 301; i++)
+            {
+                output += FizzBuzz(i);
+                if (i < 300)
+                {
+                    output += "\n";
+                }
+            }
+
+            return output;
+        }
+
+        public static string FizzBuzz(int number)
+        {
+            string output = number.ToString();
+            bool fizz = false;
+            bool buzz = false;
+
+            if (number.ToString().Contains("3") || number % 3 == 0)
+            {
+                fizz = true;
+            }
+
+            if (number.ToString().Contains("5") || number % 5 == 0)
+            {
+                buzz = true;
+            }
+
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                output = "FizzBuzz";
+            }
+            else if (fizz)
+            {
+                output = "Fizz";
+            }
+            else if (buzz)
+            {
+                output = "Buzz";
+            }
+
+            return output;
+        }
+
+        public static void WriteToFile(string contents)
+        {
+            string destinationDirectory = GetDestinationDirectory();
+            string destination = Path.Combine(destinationDirectory, "FizzBuzz.txt");
+            using (StreamWriter sw = new StreamWriter(destination, false))
+            {
+                sw.WriteLine(contents);
+            }
+
+        }
+        public static string GetDestinationDirectory()
+        {
+            string directory = Directory.GetCurrentDirectory();
+            List<string> directoryContents = new List<string>(Directory.GetFiles(directory));
+
+            while (directoryContents.FindAll(readme => readme.Contains("README.md")).Count < 1)
+            {
+                directory = Directory.GetParent(directory).FullName;
+                directoryContents = new List<string>(Directory.GetFiles(directory));
+            }
+
+            return directory;
         }
     }
 }
